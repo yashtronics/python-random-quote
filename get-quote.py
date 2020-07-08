238235171297dea782d87fbc9dac888835aa8300
yastronics <yash.puneet02@gmail.com>
2020-07-08 10:37:59 +0800

    Quote Bot Update

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,26 @@
 import random
 
 last = 13
-rnd = random.randint(0, last)
 
 def start():
   print("Keep it logically awesome.")
+
+  num = int(input("Number of quotes: "))
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes[rnd])
+  for q in range (0, num):
+    rnd = random.randint(0, last)
+    print(quotes[rnd].rstrip())
+
+def addquote():
+  quote = input("Quote to add: ")
+  f = open("quotes.txt", "w")
+  f.write(quote)
+  f.close()
 
 if __name__== "__main__":
   start()
+  addquote()
